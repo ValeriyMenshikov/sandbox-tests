4e544561ebb0ff7f101fb4014e2af06f2eec0eee
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2025-01-10 16:05:28 +0300

    добавил тест на двойную регистрацию

--- tests/register/smoke/test_post_user_register.py
+++ tests/register/smoke/test_post_user_register.py
@@ -11,3 +11,19 @@ async def test_post_user_register(
         )
     )
     assert response
+
+
+async def test_post_user_register_twice(
+    register_service_account_api: register_service.AccountApi, prepare_user_data: User
+) -> None:
+    await register_service_account_api.register_user_register_post(
+        registration=register_service.Registration(
+            email=prepare_user_data.email, password=prepare_user_data.password, login=prepare_user_data.login
+        )
+    )
+    response = await register_service_account_api.register_user_register_post(
+        registration=register_service.Registration(
+            email=prepare_user_data.email, password=prepare_user_data.password, login=prepare_user_data.login
+        )
+    )
+    assert response
